fff50bd400fddaaa80c56e156cfeb286446fd65e
withstand <chenqin1980@gmail.com>
2014-06-10 19:49:55 +0100

    Add Contents.m to summarize functions in different categories.

---
+++ Contents.m
@@ -0,0 +1,9 @@
+% Gaussian modelling based MOEA/D-DE.
+%   moeadde             - Differential evolutionary MOEA/D.
+%
+% Helper functions
+%   wsdesign            - Produce weight vectors for MOEA/D. 
+%   getfieldwithdefault - Get structure field with default value. 
+
+%   Copyright 2013 University of Essex. 
+%   $Revision: 0.1 $  $Date: 2013/10/06 19:47:00 $
