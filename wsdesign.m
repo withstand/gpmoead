3d70bea199c3b7f7c2474068219e97afc2feced3
Qin Chen <chenqin1980@gmail.com>
2014-06-12 17:43:00 +0100

    style modification

--- wsdesign.m
+++ wsdesign.m
@@ -1,11 +1,13 @@
 function [x, actualLength] = wsdesign(m,n)
 
+% 
 nn=2;
 while nchoosek(nn+n-1,n-1) < m
     nn = nn + 1;
 end
 
 x = dist_m_n(nn,n) / nn;
+
 actualLength = nchoosek(nn+n-1,n-1);
 if actualLength ~= m
     fprintf('Request weights length %d, actual weights length %d.\n', m, nchoosek(nn+n-1,n-1));
@@ -13,6 +15,8 @@ end
 
 
 end
+
+%% 
 function dis = dist_m_n(m,n)
 if n==1
     dis = m;
